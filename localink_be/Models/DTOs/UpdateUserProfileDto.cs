dda51fdea107faddb65b171f85ea3920c234c59b
aadarshreddydepa <aadarshreddydepa@gmail.com>
2026-04-01 23:29:35 +0530

    Aligned profile components to left nad made email as editable

--- localink_be/Models/DTOs/UpdateUserProfileDto.cs
+++ localink_be/Models/DTOs/UpdateUserProfileDto.cs
@@ -6,6 +6,10 @@ public class UpdateUserProfileDto
     [MaxLength(100)]
     public string FullName { get; set; } = "";
 
+    [EmailAddress]
+    [MaxLength(100)]
+    public string? Email { get; set; }
+
     [Phone]
     [MaxLength(15)]
     public string? Phone { get; set; }
